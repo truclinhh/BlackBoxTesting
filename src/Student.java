399cd526276c47d1249f09024b9e023df9b700c4
truclinhh <truclinhtranngoc.0505@gmail.com>
2022-10-08 22:24:10 +0700

    Version 1.1

--- src/Student.java
+++ src/Student.java
@@ -6,8 +6,10 @@ public class Student {
         this.midTermScore = midTermScore;
         this.absentLectures = absentLectures;
     }
-
     public int validate(){
+        if (this.midTermScore < 0 || this.midTermScore > 10
+                || this.absentLectures <0 || this.absentLectures > 15)
+            return -1;
         if (this.midTermScore <= 10 && this.midTermScore >=4
                 && this.absentLectures <=2 && this.absentLectures >=0)
             return 1;
