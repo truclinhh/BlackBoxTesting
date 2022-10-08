36d77f7b644e2a306f35414642710710583824e6
truclinhh <truclinhtranngoc.0505@gmail.com>
2022-10-08 23:04:47 +0700

    Version 2.0

--- src/Student.java
+++ src/Student.java
@@ -10,8 +10,8 @@ public class Student {
         if (this.midTermScore < 0 || this.midTermScore > 10
                 || this.absentLectures <0 || this.absentLectures > 15)
             return -1;
-        if (this.midTermScore <= 10 && this.midTermScore >=4
-                && this.absentLectures <=2 && this.absentLectures >=0)
+        if (this.midTermScore < 10 && this.midTermScore >=4
+                && this.absentLectures <2 && this.absentLectures >=0)
             return 1;
         return 0;
     }
