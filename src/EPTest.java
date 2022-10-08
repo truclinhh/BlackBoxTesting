4a9e258e6e8d911368020fb3bef784d6a8455b82
truclinhh <truclinhtranngoc.0505@gmail.com>
2022-10-08 23:12:35 +0700

    Strong Equivalence Partitioning Test

--- src/EPTest.java
+++ src/EPTest.java
@@ -13,4 +13,28 @@ public class EPTest extends TestCase {
         Student stu = new Student(8, 7);
         assertEquals(0, stu.validate());
     }
+    public void test4() {
+        Student stu = new Student(13,1);
+        assertEquals(-1, stu.validate());
+    }
+    public void test5() {
+        Student stu = new Student(13,7);
+        assertEquals(-1, stu.validate());
+    }
+    public void test6() {
+        Student stu = new Student(3, -2);
+        assertEquals(-1, stu.validate());
+    }
+    public void test7() {
+        Student stu = new Student(3, 7);
+        assertEquals(0, stu.validate());
+    }
+    public void test8() {
+        Student stu = new Student(8, -2);
+        assertEquals(-1, stu.validate());
+    }
+    public void test9() {
+        Student stu = new Student(8, 1);
+        assertEquals(1, stu.validate());
+    }
 }
